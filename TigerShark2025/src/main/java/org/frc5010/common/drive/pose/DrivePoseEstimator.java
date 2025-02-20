2b300da72d066fcebd60bfc1e12888f4d0a7ac4a
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2025-02-20 08:25:06 -0500

    Pose Updating Booleans Added

--- TigerShark2025/src/main/java/org/frc5010/common/drive/pose/DrivePoseEstimator.java
+++ TigerShark2025/src/main/java/org/frc5010/common/drive/pose/DrivePoseEstimator.java
@@ -268,6 +268,8 @@ public class DrivePoseEstimator extends GenericSubsystem {
           } 
         }
       }  
+      SmartDashboard.putBoolean("April Tag Pose Updating", visionUpdated);
+      SmartDashboard.putBoolean("Acceptor Pose Updating", accepterUpdating);
       aprilTagVisible = visionUpdated;
       updatingPoseAcceptor = accepterUpdating;
   }
