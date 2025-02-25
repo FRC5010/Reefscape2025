8eef994f7dd859d4f1e167ee2fb74a23c271dac9
Adam-Kolt <adamkoltuniuk@gmail.com>
2025-02-24 23:22:57 -0500

    Reef Avoidance Optimization and Continuous Algorithm

--- TigerShark2025/src/main/java/frc/robot/RobotModel.java
+++ TigerShark2025/src/main/java/frc/robot/RobotModel.java
@@ -15,6 +15,7 @@ import org.frc5010.common.drive.swerve.YAGSLSwerveDrivetrain;
 
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.ExponentialProfile;
@@ -112,6 +113,60 @@ public class RobotModel {
             if (distance < allowableDistance) {
                 if (distance < allowableDistance + maxObstacleDimensionDeviation + maxRobotDimensionDeviation
                         || isToCloseToVertices(newRobotPose, unavoidableVertices, robotRadius)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean linesIntersect(Translation2d line1Start, Translation2d line1End, Translation2d line2Start,
+            Translation2d line2End) {
+        double x1 = line1Start.getX(), y1 = line1Start.getY(), x2 = line1End.getX(), y2 = line1End.getY();
+        double x3 = line2Start.getX(), y3 = line2Start.getY(), x4 = line2End.getX(), y4 = line2End.getY();
+        double denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (denominator == 0) {
+            return false;
+        }
+        double xNumerator = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
+        double yNumerator = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+        double x = xNumerator / denominator;
+        double y = yNumerator / denominator;
+        return x >= Math.min(x1, x2) && x <= Math.max(x1, x2) && y >= Math.min(y1, y2) && y <= Math.max(y1, y2)
+                && x >= Math.min(x3, x4) && x <= Math.max(x3, x4) && y >= Math.min(y3, y4) && y <= Math.max(y3, y4);
+    }
+
+    private static Translation2d[] constructCornerPoints(Pose2d pose, Distance width, Distance length) {
+        Translation2d[] cornerPoints = new Translation2d[4];
+        cornerPoints[0] = pose.transformBy(new Transform2d(width.div(2), length.div(2), new Rotation2d())).getTranslation();
+        cornerPoints[1] = pose.transformBy(new Transform2d(width.div(2), length.div(-2), new Rotation2d())).getTranslation();
+        cornerPoints[2] = pose.transformBy(new Transform2d(width.div(-2), length.div(-2), new Rotation2d())).getTranslation();
+        cornerPoints[3] = pose.transformBy(new Transform2d(width.div(-2), length.div(2), new Rotation2d())).getTranslation();
+        return cornerPoints;
+    }
+
+
+    private static Translation2d[][] optimizeEdgeChecks(Translation2d[] convexPolygon, Pose2d startPose, Pose2d endPose, Distance robotRadius) {
+
+        // TODO: Don't return edges which are impossble to intersect with the robot
+        Translation2d[][] edges = new Translation2d[convexPolygon.length][2];
+        for (int i = 0; i < convexPolygon.length; i++) {
+            edges[i][0] = convexPolygon[i];
+            edges[i][1] = convexPolygon[(i + 1) % convexPolygon.length];
+        }
+        return edges;
+        
+    }
+
+    public boolean robotHasLinearPath(Pose2d robotPose, Pose2d targetPose, Translation2d[] convexPolygon, Distance robotWidth, Distance robotLength) {
+        Translation2d[] robotCornerPoints = constructCornerPoints(robotPose, robotWidth, robotLength);
+        Translation2d[] targetCornerPoints = constructCornerPoints(targetPose, robotWidth, robotLength);
+        
+        Translation2d[][] obstacleEdges = optimizeEdgeChecks(convexPolygon, robotPose, targetPose, robotWidth);
+
+        for (Translation2d[] edge : obstacleEdges) {
+            for (int i = 0; i < robotCornerPoints.length; i++) {
+                if (linesIntersect(robotCornerPoints[i], targetCornerPoints[i], edge[0], edge[1])) {
                     return false;
                 }
             }
