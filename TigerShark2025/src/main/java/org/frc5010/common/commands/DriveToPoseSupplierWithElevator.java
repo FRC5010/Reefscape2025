77f015c17e4a66f363156cef0e82e5edb895898b
mellipar000 <mellipar000@hsestudents.org>
2025-03-24 09:49:22 -0400

    Reef PID Improvements

--- TigerShark2025/src/main/java/org/frc5010/common/commands/DriveToPoseSupplierWithElevator.java
+++ TigerShark2025/src/main/java/org/frc5010/common/commands/DriveToPoseSupplierWithElevator.java
@@ -37,6 +37,12 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
   private SwerveDrivetrain swerveSubsystem;
 
   private DisplayValuesHelper displayValuesHelper = new DisplayValuesHelper("PID Values", logPrefix);
+  private DisplayDouble rotationkP;
+  private DisplayDouble rotationkD;
+
+  private final ProfiledPIDController thetaController;
+  private final GenericPID pidRotation = new GenericPID(4.0, 0, 0);
+  private final TrapezoidProfile.Constraints thetaConstraints;
 
   private int onTargetCounter = 0;
 
@@ -62,12 +68,18 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
 
   private final double maxAngularSpeed = 3.14;
   private final double MAX_ANGULAR_ACCELERATION = 6.0;
-  private final double MAX_VELOCITY = 4;
   private final double MAX_ORDINAL_ACCELERATION = 6.0;
+  private final double MAX_VELOCITY_DEVIATION = 0.1;
+  private final double MIN_VELOCITY = 0.2;
+  private final double MIN_FF_RADIUS = 0.05;
+  private final double MAX_FF_RADIUS = 0.1;
+  private final double VERTICAL_TOLERANCE = 0.03;
+  private final double HORIZONTAL_TOLERANCE = 0.03;
 
   private double forwardA = 0.0, forwardB = 0.0, forwardC = 0.0, backwardA = 0.0, backwardB = 0.0, backwardC = 0.0, horizontalA = 0.0, horizontalB = 0.0, horizontalC = 0.0, verticalA = 0.0, verticalB = 0.0, verticalC = 0.0;
   private double horizontalElevatorEngagementDistance = 0.0, verticalElevatorEngagementDistance = 0.0, horizontalStartBrakingDistance = 0.0, verticalStartBrakingDistance = 0.0, horizontalElevatorVelocityChange = 0.0, verticalElevatorVelocityChange = 0.0, elevatorAverageVelocity = 0.0, initialElevatorReference = 0.0, maxHorizontalAcceleration = 0.0, maxVerticalAcceleration = 0.0, elevatorLowerBoundTime = 0.0, elevatorExtensionTime = 0.0, initialHeight = 0.0, finalHeight = 0.0, horizontalDistance = 0.0, verticalDistance = 0.0; // Everything is in meters and seconds
-  private double verticalVelocity = 0.0, horizontalVelocity = 0.0, previousTime = 0.0, deltaTime = 0.0;
+  private double verticalVelocity = 0.0, horizontalVelocity = 0.0, thetaSpeed = 0.0, previousTime = 0.0, deltaTime = 0.0, ffInclusionFactor = 0.0;
+  private ChassisSpeeds robotChassisSpeeds = new ChassisSpeeds();
 
   /**
    * Creates a new DriveToPosition command.
@@ -88,6 +100,19 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
     this.targetPoseProvider = targetPoseProvider;
     this.elevator = elevator;
     this.velocitySupplier = robotRelativeSpeedSupplier;
+
+    thetaConstraints = new TrapezoidProfile.Constraints(maxAngularSpeed, MAX_ANGULAR_ACCELERATION);
+    thetaController = new ProfiledPIDController(pidRotation.getkP(), pidRotation.getkI(), pidRotation.getkD(), thetaConstraints);
+    thetaController.setTolerance(Units.degreesToRadians(2));
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    rotationkP = displayValuesHelper.makeConfigDouble("Rotation kP");
+    rotationkD = displayValuesHelper.makeConfigDouble("Rotation kD");
+    if (rotationkP.getValue() == 0) {
+      rotationkP.setValue(pidRotation.getkP());
+    }
+    if (rotationkD.getValue() == 0) {
+      rotationkD.setValue(pidRotation.getkD());
+    }
 
     // Setting up directional acceleration constraints
     forwardA = elevator.getForwardAccelerationConstants()[0];
@@ -122,6 +147,7 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
 
   private void updateTargetPose(Pose2d pose) {
     targetPose = pose;
+    thetaController.setGoal(targetPose.getRotation().getRadians());
     swerveSubsystem.getPoseEstimator().setTargetPoseOnField(targetPose, "Target Pose");
   }
 
@@ -148,16 +174,43 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
 
   public void engageMaximumAcceleration() {
     if (verticalStartBrakingDistance > verticalDistance) {
-      verticalVelocity -= (MAX_ORDINAL_ACCELERATION * deltaTime);
+      verticalVelocity -= Math.signum(verticalDistance) * (MAX_ORDINAL_ACCELERATION * deltaTime);
     } else if (verticalStartBrakingDistance < verticalDistance) {
-      verticalVelocity += (MAX_ORDINAL_ACCELERATION * deltaTime);
+      verticalVelocity += Math.signum(verticalDistance) * (MAX_ORDINAL_ACCELERATION * deltaTime);
     }
 
     if (horizontalStartBrakingDistance > horizontalDistance) {
-      horizontalVelocity -= Math.signum(horizontalVelocity) * (MAX_ORDINAL_ACCELERATION * deltaTime);
+      horizontalVelocity -= Math.signum(horizontalDistance) * (MAX_ORDINAL_ACCELERATION * deltaTime);
     } else if (horizontalStartBrakingDistance < horizontalDistance) {
-      horizontalVelocity += Math.signum(horizontalVelocity) * (MAX_ORDINAL_ACCELERATION * deltaTime);
+      horizontalVelocity += Math.signum(horizontalDistance) * (MAX_ORDINAL_ACCELERATION * deltaTime);
     }
+  }
+
+  public double getMaximumAcceleration(double a, double b, double c) {
+    return (a * Math.pow(elevator.getElevatorPosition().in(Meters), b)) + c;
+  }
+
+  public void limitVelocityToVelocityCurve() {
+    if (Math.abs(verticalVelocity) + (maxVerticalAcceleration * deltaTime) > verticalElevatorVelocityChange) {
+      verticalVelocity = Math.signum(verticalDistance) * verticalElevatorVelocityChange;
+    } else {
+      verticalVelocity += Math.signum(verticalDistance) * (maxVerticalAcceleration * deltaTime);
+    }
+    if (Math.abs(horizontalVelocity) + (maxHorizontalAcceleration * deltaTime) > horizontalElevatorEngagementDistance) {
+      horizontalVelocity = Math.signum(horizontalDistance) * horizontalElevatorVelocityChange;
+    } else {
+      horizontalVelocity += Math.signum(horizontalDistance) * (maxHorizontalAcceleration * deltaTime);
+    }
+    verticalVelocity = Math.signum(verticalDistance) * Math.max(Math.abs(verticalVelocity), MIN_VELOCITY);
+    horizontalVelocity = Math.signum(horizontalDistance) * Math.max(Math.abs(horizontalVelocity), MIN_VELOCITY);
+  }
+
+  public boolean verticalControllerAtTarget() {
+    return Math.abs(verticalDistance) < VERTICAL_TOLERANCE;
+  }
+
+  public boolean horizontalControllerAttarget() {
+    return Math.abs(horizontalDistance) < HORIZONTAL_TOLERANCE;
   }
 
   // Called when the command is initially scheduled.
@@ -165,8 +218,11 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
   public void init() {
     Pose2d robotPose = poseProvider.get();
     onTargetCounter = 0;
-    Translation2d toTarget = getVectorToTarget();
     currentVelocity = new Translation2d(velocitySupplier.get().vxMetersPerSecond, velocitySupplier.get().vyMetersPerSecond);
+    thetaController.reset(robotPose.getRotation().getRadians(), velocitySupplier.get().omegaRadiansPerSecond);
+
+    thetaController.setP(rotationkP.getValue());
+    thetaController.setD(rotationkD.getValue()); 
     
     goingToReef = targetPoseProvider.get().getTranslation().getDistance(centerOfReef.getTranslation()) < 2.0;
     verticalA = goingToReef ? backwardA : forwardA;
@@ -180,18 +236,6 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
     verticalVelocity = currentVelocity.getX();
     horizontalVelocity = currentVelocity.getY();
     updateVitalCalculations();
-
-    double initialSpeedTowardsTarget = Math.min(
-        0.0,
-        -currentVelocity
-            .rotateBy(
-                targetPoseProvider
-                    .get()
-                    .getTranslation()
-                    .minus(robotPose.getTranslation())
-                    .getAngle()
-                    .unaryMinus())
-            .getX());
 
     if (null != targetPoseProvider.get()) {
       updateTargetPose(targetPoseProvider.get().transformBy(targetTransform));
@@ -211,50 +255,50 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
       updateTargetPose(target); // Add conditional to update the target pose
     }
 
+    thetaSpeed = thetaController.calculate(robotPose2d.getRotation().getRadians());
     currentVelocity = new Translation2d(velocitySupplier.get().vxMetersPerSecond, velocitySupplier.get().vyMetersPerSecond);
     forwardVector = new Translation2d(1.0, 0.0).rotateBy(robotPose2d.getRotation().plus(new Rotation2d(Degrees.of(180))));
     rightVector = new Translation2d(0.0, 1.0).rotateBy(robotPose2d.getRotation().plus(new Rotation2d(Degrees.of(180)))); // TO-DO: Ensure this code rotates the vectors correctly
     pathVector = targetPose.getTranslation().minus(robotPose2d.getTranslation());
+    ffInclusionFactor = MathUtil.clamp((pathVector.getNorm() - MIN_FF_RADIUS) / (MAX_FF_RADIUS - MIN_FF_RADIUS), 0.0, 1.0);
     verticalDistance = (forwardVector.getX() * pathVector.getX()) + (forwardVector.getY() * pathVector.getY());
     horizontalDistance = (rightVector.getX() * pathVector.getX()) + (rightVector.getY() * pathVector.getY());
     elevatorLowerBoundTime = (elevator.getElevatorPosition().in(Meters) - initialHeight) / elevatorAverageVelocity;
+    maxVerticalAcceleration = getMaximumAcceleration(verticalA, verticalB, verticalC);
+    maxHorizontalAcceleration = getMaximumAcceleration(horizontalA, horizontalB, horizontalC);
 
     deltaTime = (System.nanoTime() / 1E9) - previousTime;
     previousTime = System.nanoTime() / 1E9;
-    if ((verticalDistance < verticalElevatorEngagementDistance && Math.abs(verticalVelocity) - verticalElevatorVelocityChange > 0) || (horizontalDistance < horizontalElevatorEngagementDistance && Math.abs(horizontalVelocity) - horizontalElevatorVelocityChange > 0)) {
-      elevatorExtensionTime -= deltaTime;
+    if ((verticalDistance < verticalElevatorEngagementDistance && Math.abs(verticalVelocity) - (verticalElevatorVelocityChange + (maxVerticalAcceleration * deltaTime)) > MAX_VELOCITY_DEVIATION) || (horizontalDistance < horizontalElevatorEngagementDistance && Math.abs(horizontalVelocity) - (horizontalElevatorVelocityChange + (maxHorizontalAcceleration * deltaTime)) > MAX_VELOCITY_DEVIATION)) {
+      elevatorExtensionTime -= deltaTime; // TO-DO: Consider reseting the extension time upon stopping the elevator
       elevator.stopElevator();
       engageMaximumAcceleration();
-    }  else if (verticalDistance < verticalElevatorEngagementDistance || horizontalDistance < horizontalElevatorEngagementDistance) {
+    }  else if (verticalDistance < verticalElevatorEngagementDistance && horizontalDistance < horizontalElevatorEngagementDistance) {
       elevator.pidControlCommand(Meters.of(finalHeight));
-      // TO-DO: Add horizontal and vertical velocity curve limitation
+      limitVelocityToVelocityCurve();
     } else {
       engageMaximumAcceleration();
     }
     updateVitalCalculations();
 
-    double distanceToGoal = getDistanceToTarget();
-    double translationalSpeed = 0; // Add logic
-    Rotation2d movementAngle = getAngleToTarget();
-    double speedX = movementAngle.getCos() * translationalSpeed;
-    double speedY = movementAngle.getSin() * translationalSpeed;
     double thetaSpeed = 0.0;
 
-    ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(), new Rotation2d());
+    robotChassisSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(verticalVelocity, horizontalVelocity, thetaSpeed + (thetaController.getSetpoint().velocity * ffInclusionFactor), swerveSubsystem.getHeading());
 
-    SmartDashboard.putNumber("X Speed", chassisSpeeds.vxMetersPerSecond);
-    SmartDashboard.putNumber("Y Speed", chassisSpeeds.vyMetersPerSecond);
-    SmartDashboard.putNumber("Theta Speed", chassisSpeeds.omegaRadiansPerSecond);
-    SmartDashboard.putBoolean("Distance Controller At Setpoint", false); // TO-D0: Fix
-    SmartDashboard.putBoolean("Theta Controller at Setpoins", false); // TO-D0: Fix
-    swerveSubsystem.drive(chassisSpeeds, null);
+    SmartDashboard.putNumber("Vertical Velocity", robotChassisSpeeds.vxMetersPerSecond);
+    SmartDashboard.putNumber("Horizontal Velocity", robotChassisSpeeds.vyMetersPerSecond);
+    SmartDashboard.putNumber("Theta Speed", robotChassisSpeeds.omegaRadiansPerSecond);
+    SmartDashboard.putBoolean("Vertical Controller At Setpoint", verticalControllerAtTarget());
+    SmartDashboard.putBoolean("Horizontal Controller At Setpoint", horizontalControllerAttarget());
+    SmartDashboard.putBoolean("Theta Controller at Setpoint", thetaController.atGoal());
+    swerveSubsystem.drive(robotChassisSpeeds, null);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void stop(boolean interrupted) {
     onTargetCounter = 0;
-    SmartDashboard.putBoolean("DriveToPositionInterrupted", interrupted);
+    SmartDashboard.putBoolean("DriveToPositionWithElevatorInterrupted", interrupted);
     swerveSubsystem.drive(
         new ChassisSpeeds(0, 0, 0), null);
   }
@@ -262,7 +306,7 @@ public class DriveToPoseSupplierWithElevator extends GenericCommand {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (true) { // TO-DO: Add at goal logic
+    if (verticalControllerAtTarget() && horizontalControllerAttarget() && thetaController.atGoal()) {
       onTargetCounter++;
     }
     return onTargetCounter > 5;
